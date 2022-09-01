9e15dc80591f4a648f9ad6d121e6be7157fce8c3
Jackson <dmitrievei92@gmail.com>
2022-09-01 22:20:46 +0300

    dbupdate

--- EmployeeService/AccountHelper/Program.cs
+++ EmployeeService/AccountHelper/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            Console.WriteLine(PasswordUtils.CreatePasswordHash("12345"));
         }
     }
 }
